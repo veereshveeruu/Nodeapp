cafa46b0e5d4df05beaa656c4f98c48735a1f32a
root <root@ip-172-31-87-35.ec2.internal>
2025-06-28 16:23:11 +0000

    merge conflict resolve

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => 
   res.send('🚀 Hello from Node.js CI/CD Pipeline App! hello veeresh you can logoff now ');
 });
 
